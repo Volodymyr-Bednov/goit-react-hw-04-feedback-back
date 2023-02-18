83ef78fbac089331b1a708e048f51d8625ff68db
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2023-02-18 19:41:05 +0200

    refactoring-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,45 @@
-import { Component } from 'react';
+import { useState , useEffect } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import css from './App.module.css';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positivePercentage: 0,
-    isVisible: false,
-  };
 
-  LeaveFeedback = evt => {
-    if (evt.target.type !== 'button') return;
-    const selectGrade = evt.target.value;
-    this.setState(prevValue => {
-      return {
-        [selectGrade]: prevValue[selectGrade] + 1,
-        isVisible: true,
-      };
-    });
-    this.countTotalFeedback();
-  };
+export const App = () => {
+  const [commentsGood, setCommentsGood] = useState(0);
+  const [commentsNeutral, setCommentsNeutral] = useState(0);
+  const [commentsBad, setCommentsBad] = useState(0);
+  const [commentsTotal, setCommentsTotal] = useState(0);
+  const [positiveCommentsPercentage, setPositiveCommentsPercentage] = useState(0);
+  const [isVisible, setIsVisible] = useState(0);
+  
+useEffect(()=>{
+  setCommentsTotal( commentsGood + commentsNeutral + commentsBad);
+  
+  if ( commentsGood || commentsNeutral || commentsBad) setIsVisible(1);
+}, [commentsGood, commentsNeutral, commentsBad])
 
-  countTotalFeedback = () => {
-    this.setState(prevValue => {
-      const { good, neutral, bad } = prevValue;
-      return { total: good + neutral + bad };
-    });
-    this.countPositiveFeedbackPercentage();
-  };
+useEffect(()=>{
+if (commentsGood) setPositiveCommentsPercentage(  Math.round((commentsGood * 100) / commentsTotal)  )
 
-  countPositiveFeedbackPercentage() {
-    this.setState(prevValue => {
-      const { good, total } = prevValue;
-      return { positivePercentage: Math.round((good * 100) / total) };
-    });
-  }
+}, [commentsGood , commentsTotal])
 
-  render() {
-    return (
-      <div className={css.wrap}>
+  return(
+          <div className={css.wrap}>
         <Section
           title={'Please leave feedback'}
-          children={<FeedbackOptions onLeaveFeedback={this.LeaveFeedback} />}
+           children={<FeedbackOptions data ={{setCommentsGood, setCommentsNeutral, setCommentsBad}}/>}
         />
-        {/* <FeedbackOptions onLeaveFeedback={this.LeaveFeedback} /> */}
-
         <Section
           title={'Statistics'}
           children={
-            this.state.isVisible ? (
+            isVisible ? (
               <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.state.total}
-                positivePercentage={this.state.positivePercentage}
+                good={commentsGood}
+                neutral={commentsNeutral}
+                bad={commentsBad}
+                total={commentsTotal}
+                positivePercentage={positiveCommentsPercentage}
               />
             ) : (
               <Notification message={'There is no feedback'} />
@@ -67,6 +47,5 @@ export class App extends Component {
           }
         />
       </div>
-    );
-  }
-}
+  )
+} 
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types';
 import css from './Feedback.module.css';
-export const FeedbackOptions = ({ onLeaveFeedback }) => {
+export const FeedbackOptions = ({data:{setCommentsGood, setCommentsNeutral, setCommentsBad}}) => {
+  const FeedbackPressButton = (evt) =>{
+    if (evt.target.type !== 'button') return;
+    switch (evt.target.value) {
+      case 'good': setCommentsGood( prevState => prevState +1);
+      break;
+      case 'neutral': setCommentsNeutral( prevState => prevState +1);;
+      break;
+      case 'bad': setCommentsBad( prevState => prevState +1);;
+      break;
+      default: console.log('default');
+    }
+
+  }
   return (
     <div>
-      <div className={css.grade} onClick={onLeaveFeedback}>
+      <div className={css.grade} onClick={FeedbackPressButton}>
         <button className={css.btn} type="button" value="good">
           Good
         </button>
